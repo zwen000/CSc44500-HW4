d5262d8fb486429f21e6e085ed875b27ebaf5ccc
zhicong wen <37707613+zwen000@users.noreply.github.com>
2020-11-16 16:48:21 -0500

    Add files via upload

---
+++ BDM_HW4_Wen2.py
@@ -0,0 +1,28 @@
+from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
+import sys
+import csv
+
+if __name__ == '__main__':
+    def parseCSV(idx, part):
+        if idx == 0:
+            next(part)
+        for p in csv.reader(part):
+            yield ("\"" + p[1] + "\"", p[0].split("-")[0], p[7])
+
+
+    path_in = "complaints_small.csv" if len(sys.argv) > 1 else sys.argv[1]
+    path_out = "hw4_output" if len(sys.argv) > 2 else sys.argv[2]
+    sc = SparkContext.getOrCreate()
+    # sqlContext = SQLContext(sc)
+    rows = sc.textFile(path_in).mapPartitionsWithIndex(parseCSV) \
+        .map(lambda x: ((x[0], x[1], x[2]), 1)) \
+        .reduceByKey(lambda x, y: x + y) \
+        .map(lambda x: (x[0][:2], x[1])) \
+        .groupByKey() \
+        .mapValues(lambda x: (str(sum(x)), str(len(x)), str(round((max(x) / sum(x) * 100))))) \
+        .sortByKey() \
+        .map(lambda x: (x[0] + x[1])) \
+        .map(lambda x: (", ".join(x))) \
+        .collect()
+    sc.parallelize(rows).saveAsTextFile(path_out)
