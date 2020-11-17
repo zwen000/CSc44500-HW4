666c7c26a788bcb4eb7d7840a5e4d47700e595f5
zhicong wen <37707613+zwen000@users.noreply.github.com>
2020-11-17 00:10:52 -0500

    Update BDM_HW4_Wen2.py

--- BDM_HW4_Wen2.py
+++ BDM_HW4_Wen2.py
@@ -1,28 +1,27 @@
 from pyspark import SparkContext
-from pyspark.sql.session import SparkSession
 import sys
 import csv
 
-if __name__ == '__main__':
+if __name__=='__main__':
     def parseCSV(idx, part):
         if idx == 0:
             next(part)
         for p in csv.reader(part):
-            yield ("\"" + p[1] + "\"", p[0].split("-")[0], p[7])
+            yield ("\""+p[1]+"\"", p[0].split("-")[0], p[7])
 
-
-    path_in = "complaints_small.csv" if len(sys.argv) > 1 else sys.argv[1]
-    path_out = "hw4_output" if len(sys.argv) > 2 else sys.argv[2]
+    path_in = sys.argv[1] if len(sys.argv)>1 else "complaints_small.csv"
+    path_out = sys.argv[2] if len(sys.argv)>2 else "hw4_output"
     sc = SparkContext.getOrCreate()
-    # sqlContext = SQLContext(sc)
-    rows = sc.textFile(path_in).mapPartitionsWithIndex(parseCSV) \
-        .map(lambda x: ((x[0], x[1], x[2]), 1)) \
-        .reduceByKey(lambda x, y: x + y) \
-        .map(lambda x: (x[0][:2], x[1])) \
-        .groupByKey() \
-        .mapValues(lambda x: (str(sum(x)), str(len(x)), str(round((max(x) / sum(x) * 100))))) \
-        .sortByKey() \
-        .map(lambda x: (x[0] + x[1])) \
-        .map(lambda x: (", ".join(x))) \
-        .collect()
+
+    rows= sc.textFile(path_in, use_unicode=False).mapPartitionsWithIndex(parseCSV)\
+            .map(lambda x: ((x[0], x[1], x[2]), 1))\
+            .reduceByKey(lambda x,y: x+y)\
+            .map(lambda x:(x[0][:2],x[1]))\
+            .groupByKey()\
+            .mapValues(lambda x: (str(sum(x)), str(len(x)), str(int(float(max(x))/sum(x)*100))))\
+            .sortByKey()\
+            .map(lambda x:(x[0]+x[1]))\
+            .map(lambda x: (", ".join(x)))\
+            .collect()
     sc.parallelize(rows).saveAsTextFile(path_out)
+
